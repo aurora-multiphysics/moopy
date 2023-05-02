0d963a23c77f08e0a048e336ee3e3992202bef27
amna-khan <amna.khan@ukaea.uk>
2023-05-02 22:17:51 +0100

    i think the kwargs are writte correctly now

--- moose/variables.py
+++ moose/variables.py
@@ -27,33 +27,27 @@ class Family(IntEnum):
     RATIONAL_BERNSTEIN = auto()
     SIDE_HIERARCHIC = auto()
     
-class Variable():
-    def __init__(self, name="", order=None, family=None, block=None, initial_condition=None):
+class Variable:
+    def __init__(self, name, order=None, **kwargs):
         self.name = name
         self.order = order
-        self.family = family
-        self.block = block
-        self.initial_condition = initial_condition
+        self.kwargs = kwargs
 
     def __str__(self):
         string = f'[{self.name}]\n'
-        if self.order != None:
-            string += f'order = {self.order.name}\n'
-        else:
-            string += f'order = {self.order}\n'
-        if self.family != None:
-            string += f'family = {self.family.name}\n'
-        else:
-            string += f'family = {self.family}\n'
-        if self.initial_condition:
-            string += f'initial_condition = "{self.initial_condition}"\n'
-        if self.block:
-            string += f'block = "{self.block}"\n'
+        if self.order is not None:
+            string += f'order = {Order(self.order).name}\n'
+        if 'family' in self.kwargs:
+            string += f'family = {self.kwargs["family"].name}\n'
+        if 'initial_condition' in self.kwargs:
+            string += f'initial_condition = "{self.kwargs["initial_condition"]}"\n'
+        if 'block' in self.kwargs and self.kwargs['block'] is not None:
+            string += f'block = "{self.kwargs["block"]}"\n'
         string += f'[]\n'
         return string
 
 class AuxVariable(Variable):
-    def __init__(self, name="",order=None, family=None, block=None, initial_condition=None):
+    def __init__(self, name="",**kwargs):
         super().__init__(name, order, family, block, initial_condition)
 
 class Variables():
@@ -61,11 +55,10 @@ class Variables():
         self.name = "Variables"
         self.variables = {}
 
-    def add_variable(self, name, order, family, block, initial_condition):
-        variable = Variable(name=name, order=order,family=family,block=block, initial_condition=initial_condition)
+    def add_variable(self, name, order=None, **kwargs):
+        variable = Variable(name=name, order=order, **kwargs)
         if variable.name in self.variables.keys():
             print("variable name already in use")
-            
         self.variables[variable.name] = variable
 
     def __str__(self):
